b948beff8b95a043fc516d6835360e38f0021f5e
joppess <h24jolid@du.se>
2025-07-23 15:09:05 +0200

    Första commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // klass = ritning (klassen Magiker)
+        // objekt = en ett riktigt hus (new Magiker = man bygger själva figuren "magikern")
+        // denna byggs utifrån ritningen/klassen.
+        Magiker m = new Magiker(25, 100, "Gandalf"); // ett nytt objekt(instans) av klassen magiker
+    }
+}
